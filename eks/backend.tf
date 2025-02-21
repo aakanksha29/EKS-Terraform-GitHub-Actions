e5664e51f1bede134970ba95bdd197c2b113cebd
aakanksha29 <47315499+aakanksha29@users.noreply.github.com>
2025-02-21 15:26:16 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,11 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
+
   backend "s3" {
     bucket         = "dev-akki-tf-bucket"
     region         = "us-east-1"
